57d0919ba79c6699fd13854164abb55ffd9f21ef
Elmira Madadi <elmiramadadi@gmail.com>
2021-04-02 18:44:11 +0430

    Created Room.java and tested

---
+++ src/main/java/se/lexicon/group/Thymeleafmvcjpahotel/entity/Room.java
@@ -0,0 +1,108 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String roomId;
+
+    private String code;
+    private Short floor;
+    private String description;
+    private Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "room_type_id",referencedColumnName = "roomTypeId")
+    private RoomType roomType;
+
+    public Room(){}
+
+    public Room(String roomId, String code, Short floor, String description, Boolean available, RoomType roomType) {
+        this.roomId = roomId;
+        this.code = code;
+        this.floor = floor;
+        this.description = description;
+        this.available = available;
+        this.roomType = roomType;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Short getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Short floor) {
+        this.floor = floor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(roomId, room.roomId) &&
+                Objects.equals(code, room.code) &&
+                Objects.equals(floor, room.floor) &&
+                Objects.equals(description, room.description) &&
+                Objects.equals(available, room.available) &&
+                Objects.equals(roomType, room.roomType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomId, code, floor, description, available, roomType);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomId='" + roomId + '\'' +
+                ", code='" + code + '\'' +
+                ", floor=" + floor +
+                ", description='" + description + '\'' +
+                ", available=" + available +
+                ", roomType=" + roomType +
+                '}';
+    }
+}
---
+++ src/test/java/se/lexicon/group/Thymeleafmvcjpahotel/entity/RoomTest.java
@@ -0,0 +1,57 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+    Room testObject;
+    RoomType roomType;
+
+    @BeforeEach
+    void setUp() {
+        roomType = new RoomType( "Room type description");
+        testObject = new Room("102", "22", (short) 2, "Your hotel room is smarter than you", true, roomType);
+
+    }
+
+    @Test
+    void successfully_created() {
+        assertNotNull(testObject);
+        assertNotEquals("Null", testObject.getRoomId());
+        assertEquals("102", testObject.getRoomId());
+        assertEquals("22", testObject.getCode());
+        assertEquals("Your hotel room is smarter than you", testObject.getDescription());
+        assertEquals((short) 2, testObject.getFloor());
+        assertEquals(true, testObject.getAvailable());
+        assertEquals(roomType, testObject.getRoomType());
+    }
+
+    @Test
+    void testEquals() {
+        RoomType roomType1 = new RoomType( "Room type description");
+        Room room1 = new Room("102", "22", (short) 2, "Your hotel room is smarter than you", true, roomType1);
+
+        assertTrue(testObject.equals(room1));
+    }
+
+    @Test
+    void testHashCode() {
+        RoomType roomType1 = new RoomType( "Room type description");
+        Room room1 = new Room("102", "22", (short) 2, "Your hotel room is smarter than you", true, roomType1);
+        assertEquals(room1.hashCode(), testObject.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        String toString = testObject.toString();
+        assertTrue(toString.contains("0"));
+        assertTrue(toString.contains("102"));
+        assertTrue(toString.contains("22"));
+        assertTrue(toString.contains("Your hotel room is smarter than you"));
+        assertTrue(toString.contains("Room type description"));
+        // assertTrue(toString.contains("55"));
+    }
+
+}

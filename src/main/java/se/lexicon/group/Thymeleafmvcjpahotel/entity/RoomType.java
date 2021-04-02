9eafde31ea4142986157a58b4bfb1c4301765a09
Elmira Madadi <elmiramadadi@gmail.com>
2021-04-02 18:35:37 +0430

    Created RoomType.java and tested

---
+++ src/main/java/se/lexicon/group/Thymeleafmvcjpahotel/entity/RoomType.java
@@ -0,0 +1,61 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class RoomType {
+
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String roomTypeId;
+
+    private String description;
+
+    public RoomType() {
+    }
+    public RoomType( String description) {
+        // this.roomTypeId = roomTypeId;
+        this.description = description;
+    }
+
+    public String getRoomTypeId() {
+        return roomTypeId;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoomType roomType = (RoomType) o;
+        return Objects.equals(roomTypeId, roomType.roomTypeId) && Objects.equals(description, roomType.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomTypeId, description);
+    }
+
+    @Override
+    public String toString() {
+        return "RoomType{" +
+                "roomTypeId='" + roomTypeId + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/se/lexicon/group/Thymeleafmvcjpahotel/entity/RoomTypeTest.java
@@ -0,0 +1,42 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RoomTypeTest {
+        RoomType testObject;
+
+        @BeforeEach
+        void setUp() {
+            testObject = new RoomType( "Your hotel room is smarter than you");
+        }
+
+        @Test
+        void successfully_created() {
+            assertNotNull(testObject);
+            // assertEquals("55", testObject.getRoomTypeId());
+            assertEquals("Your hotel room is smarter than you", testObject.getDescription());
+        }
+
+        @Test
+        void testEquals() {
+            RoomType room = new RoomType( "Your hotel room is smarter than you");
+            assertTrue(testObject.equals(room));
+        }
+
+        @Test
+        void testHashCode() {
+            RoomType room = new RoomType( "Your hotel room is smarter than you");
+            assertEquals(room.hashCode(), testObject.hashCode());
+        }
+
+        @Test
+        void testToString() {
+            String toString = testObject.toString();
+            // assertTrue(toString.contains("55"));
+            assertTrue(toString.contains("Your hotel room is smarter than you"));
+        }
+
+    }

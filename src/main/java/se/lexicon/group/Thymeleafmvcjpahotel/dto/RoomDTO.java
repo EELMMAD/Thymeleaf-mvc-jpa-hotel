ab6638d0c754f324df62dcd127fcce18c905d372
Elmira Madadi <elmiramadadi@gmail.com>
2021-04-05 18:39:07 +0430

    Created RoomRepository.java and Tested.

---
+++ src/main/java/se/lexicon/group/Thymeleafmvcjpahotel/dto/RoomDTO.java
@@ -0,0 +1,132 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.dto;
+
+
+import se.lexicon.group.Thymeleafmvcjpahotel.entity.Room;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RoomDTO implements Serializable {
+
+    private String roomId;
+    @NotBlank(message = "Required field")
+    @Size(min = 2, max = 255, message = "Need to have at least 2 letters")
+    private String code;
+
+    private RoomTypeDTO roomType;
+    private String description;
+    private Boolean available;
+
+    private Short floor;
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public RoomDTO() {
+    }
+
+    public RoomDTO(String roomId, @NotBlank(message = "Required field") @Size(min = 2, max = 255, message = "Need to have at least 2 letters") String code,
+                   RoomTypeDTO roomType, String description, Boolean available, Short floor) {
+        this.roomId = roomId;
+        this.code = code;
+        this.roomType = roomType;
+        this.description = description;
+        this.available = available;
+        this.floor = floor;
+    }
+
+    public RoomDTO(Room room) {
+        this.roomId = room.getRoomId();
+        this.code = room.getCode();
+        this.floor = room.getFloor();
+        this.roomType = new RoomTypeDTO(room.getRoomType());
+        this.description = room.getDescription();
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public RoomTypeDTO getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomTypeDTO roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public Short getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Short floor) {
+        this.floor = floor;
+    }
+    /////
+    public static List<RoomDTO> toRoomDto(List<Room> rooms) {
+        List<RoomDTO> result = new ArrayList<>();
+        for (Room room : rooms) {
+            RoomDTO roomDto = new RoomDTO(room);
+            result.add(roomDto);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoomDTO roomDTO = (RoomDTO) o;
+        return Objects.equals(roomId, roomDTO.roomId) && Objects.equals(code, roomDTO.code) && Objects.equals(roomType, roomDTO.roomType) && Objects.equals(description, roomDTO.description) && Objects.equals(available, roomDTO.available) && Objects.equals(floor, roomDTO.floor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomId, code, roomType, description, available, floor);
+    }
+
+    @Override
+    public String toString() {
+        return "RoomDTO{" +
+                "roomId='" + roomId + '\'' +
+                ", code='" + code + '\'' +
+                ", roomType=" + roomType +
+                ", description='" + description + '\'' +
+                ", available=" + available +
+                ", floor=" + floor +
+                '}';
+    }
+}
---
+++ src/test/java/se/lexicon/group/Thymeleafmvcjpahotel/dto/RoomDTOTest.java
@@ -0,0 +1,60 @@
+package se.lexicon.group.Thymeleafmvcjpahotel.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.lexicon.group.Thymeleafmvcjpahotel.entity.Room;
+import se.lexicon.group.Thymeleafmvcjpahotel.entity.RoomType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomDTOTest {
+    Room firstRoom;
+    Room secondRoom;
+    RoomType firstRoomType;
+    RoomType secondRoomType;
+    RoomDTO firstRoomDTO;
+    RoomDTO secondRoomDTO;
+    List<Room> roomList;
+
+    @BeforeEach
+    void setUp() {
+        firstRoomType = new RoomType("firstRoomDescription");
+        secondRoomType = new RoomType("secondRoomDescription");
+
+        firstRoom = new Room("102", "aa", (short) 4, "Your hotel room is smarter than you", true, firstRoomType);
+        secondRoom = new Room("100", "bb", (short) 8, "Second room description", true, secondRoomType);
+
+        firstRoomDTO = new RoomDTO(firstRoom);
+        secondRoomDTO = new RoomDTO(secondRoom);
+
+        roomList = new ArrayList<>();
+        roomList.add(firstRoom);
+        roomList.add(secondRoom);
+    }
+
+    @Test
+    void if_convert_room_to_roomDto() {
+        assertEquals(firstRoom.getRoomId(), firstRoomDTO.getRoomId());
+        assertEquals(firstRoom.getCode(), firstRoomDTO.getCode());
+        assertEquals(firstRoom.getFloor(), firstRoomDTO.getFloor());
+        assertEquals(secondRoom.getRoomId(), secondRoomDTO.getRoomId());
+        assertEquals(secondRoom.getCode(), secondRoomDTO.getCode());
+        assertEquals(secondRoom.getFloor(), secondRoomDTO.getFloor());
+        assertEquals(firstRoom.getDescription(), firstRoomDTO.getDescription());
+        assertEquals(secondRoom.getDescription(), secondRoomDTO.getDescription());
+    }
+
+    @Test
+    void if_covert_room_list_to_roomDto_list() {
+        secondRoomType = new RoomType("secondRoomDescription");
+        secondRoom = new Room("100", "bb", (short) 8, "Second room description", true, secondRoomType);
+        List<Room> roomList = new ArrayList<>();
+        roomList.add(firstRoom);
+        roomList.add(secondRoom);
+        List<RoomDTO> roomDtoList = RoomDTO.toRoomDto(roomList);
+        assertEquals(roomList.size(), roomDtoList.size());
+    }
+}
